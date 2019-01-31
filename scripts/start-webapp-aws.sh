2ab6ec8e4e3b3211f918925768f5c5056e91c19b
Vishnu Dadhich <vishnu.dadhich@mitchell.com>
2019-01-31 11:06:24 -0800

    removed logging and added --rm tag

--- scripts/start-webapp-aws.sh
+++ scripts/start-webapp-aws.sh
@@ -11,13 +11,13 @@ sudo bash get-docker.sh
 docker run --rm -d -u root -p 8080:8080 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME":/home jenkinsci/blueocean
 
 #Build the java project with Maven
-docker run -it -v "$PWD":/app -w /app maven mvn clean install | tee my-build.log
+docker run -it --rm -v "$PWD":/app -w /app maven mvn clean install 
 
 #Create the docker image with Tomcat
 docker build -f Dockerfile -t hello-world:1 .
 
 #Run the container with the created image
-docker run -d -p 80:8080 hello-world:1
+docker run --rm -d -p 80:8080 hello-world:1
 
 #Echo some random sentence
 echo "Now you can access your web application at the following url in the browser. http://localhost:80/webapp"
