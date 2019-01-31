dfcea2fec0b1ed2efe36aeba492e17edd8997979
Vishnu Dadhich <vishnu.dadhich@mitchell.com>
2019-01-31 09:38:17 -0800

    Adding aws startup scripts

---
+++ scripts/start-webapp-aws.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Update apt-get
+sudo apt-get update
+
+#Install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo bash get-docker.sh
+
+#Run Jenkins
+docker run --rm -u root -p 8080:8080 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME":/home jenkinsci/blueocean
+
+#Build the java project with Maven
+docker run -it --rm -v "$PWD":/app -w /app maven mvn clean install
+
+#Create the docker image with Tomcat
+docker build -f Dockerfile -t hello-world:1 .
+
+#Run the container with the created image
+docker run --rm -d -p 80:8080 hello-world:1
+
+#Echo some random sentence
+echo "Now you can access your web application at the following url in the browser. http://localhost:80/webapp"
