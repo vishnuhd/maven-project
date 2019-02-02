4a467c6a8323d7b034b0d76f1b01f0024259218f
Vishnu Dadhich <vishnu.dadhich@mitchell.com>
2019-02-02 01:04:59 -0800

    updating the bash script

--- scripts/start-webapp-aws.sh
+++ scripts/start-webapp-aws.sh
@@ -10,14 +10,3 @@ sudo bash get-docker.sh
 #Run Jenkins
 docker run --rm -d -u root -p 8080:8080 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME":/home jenkinsci/blueocean
 
-#Build the java project with Maven
-docker run -it --rm -v "$PWD":/app -w /app maven mvn clean install 
-
-#Create the docker image with Tomcat
-docker build -f Dockerfile -t hello-world:1 .
-
-#Run the container with the created image
-docker run --rm -d -p 80:8080 hello-world:1
-
-#Echo some random sentence
-echo "Now you can access your web application at the following url in the browser. http://localhost:80/webapp"
