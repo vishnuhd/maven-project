b47e879dd7b9f7a9786ce7e39870fbe67a54c8ac
Vishnu Dadhich <vishnu.dadhich@mitchell.com>
2019-02-02 11:51:54 -0800

    updating startup script to install Jenkins witout Docker

--- scripts/start-webapp-aws.sh
+++ scripts/start-webapp-aws.sh
@@ -9,9 +9,13 @@ sudo bash get-docker.sh
 sudo service docker start
 sudo usermod -a -G docker ubuntu
 
-#Run Jenkins
-docker run --rm -d -u root -p 8080:8080 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME":/home jenkinsci/blueocean
-
+#Install Jenkins
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
 sudo ufw allow 8080
 sudo ufw status
 sudo ufw allow OpenSSH
