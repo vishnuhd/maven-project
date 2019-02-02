85379e237afe4a8fb95c0f6246b415dc0c906136
Vishnu Dadhich <vishnu.dadhich@mitchell.com>
2019-02-02 02:23:41 -0800

    updating Jenkinsfile and startup script

--- scripts/start-webapp-aws.sh
+++ scripts/start-webapp-aws.sh
@@ -6,6 +6,8 @@ sudo apt-get update
 #Install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo bash get-docker.sh
+sudo service docker start
+sudo usermod -a -G docker ubuntu
 
 #Run Jenkins
 docker run --rm -d -u root -p 8080:8080 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME":/home jenkinsci/blueocean
