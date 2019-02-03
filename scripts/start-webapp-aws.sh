67d633b581fd8f82e6b982dfaed594e38a92e283
Vishnu Dadhich <vishnu.dadhich@mitchell.com>
2019-02-03 00:54:38 -0800

    updating startup script

--- scripts/start-webapp-aws.sh
+++ scripts/start-webapp-aws.sh
@@ -9,5 +9,4 @@ sudo bash get-docker.sh
 sudo service docker start
 
 #Run Jenkins
-docker run --rm -d -u root -p 8080:8080 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME":/home jenkinsci/blueocean
-
+docker run -u root -p 8080:8080 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME":/home jenkins/jenkins
