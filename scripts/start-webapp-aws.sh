be8689adfa894f1a6dca9f9939903266acf41574
Vishnu Dadhich <vishnu.dadhich@mitchell.com>
2019-02-03 01:38:22 -0800

    updating startup script

--- scripts/start-webapp-aws.sh
+++ scripts/start-webapp-aws.sh
@@ -7,6 +7,20 @@ sudo apt-get update
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo bash get-docker.sh
 sudo service docker start
+usermod -aG docker ubuntu
 
 #Run Jenkins
-docker run -u root -p 8080:8080 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME":/home jenkins/jenkins
+#docker run -u root -p 8080:8080 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME":/home jenkins/jenkins
+
+#Install Jenkins
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo ufw allow 8080
+sudo ufw status
+sudo ufw allow OpenSSH
+sudo ufw enable -y
+usermod -aG docker jenkins
